d359f537927220f4751188ab360d57f2ceb15ad6
Nata Pshe <nata.psh277@gmail.com>
2017-09-18 20:39:19 +0300

    added method remove by id

--- src/main/java/Pshennikova/Notebook.java
+++ src/main/java/Pshennikova/Notebook.java
@@ -23,4 +23,16 @@ public class Notebook {
     public List<Record> list(){
         return records;
     }
+
+    @Command
+    public void remove(int id) {
+        for (int i = 0; i < records.size(); i++) { // цикл по всем индексам
+            Record r = records.get(i);     // берем запись по текущему индексу
+            if (r.getId() == id) {         // если у записи айдишка совпадает с искомой
+                records.remove(i);         // удаляем по индексу
+                break;                     // заканчиваем цикл досрочно
+            }
+        }
+
+    }
 }
